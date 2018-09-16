359f15d1c0b191692c939d89a092023bb0217f99
klipan <nikola.klipa@yahoo.com>
2018-09-16 11:08:28 +0200

    commit questionnaire_page

---
+++ features/pages/questionnaire_page.py
@@ -0,0 +1,25 @@
+
+class Locators_q(object):
+
+    _path_question_second_page = "//*[contains(text(), 'Have you traded')]"
+    _path_finish_button = '[id=button-step]'
+    _path_shares = "//select[@id='form-shares']"
+    _path_forex = "//select[@id='form-forex']"
+    _path_cfds = "//select[@id='form-cfds']"
+    _path_spread_betting = "//select[@id='form-spread_betting']"
+    _path_relevante_experience = "//select[@id='form-relevant_experience']"
+    _path_trading_platform = "//select[@id='form-trading_accounts']"
+    _path_currency = "//select[@id='form-currency']"
+    _path_annual_income = "//select[@id='form-approx_annual_income']"
+    _path_employment_status = "//select[@id='form-employment_status']"
+    _path_assets = "//select[@id='form-liquid_savings']"
+    _java_script_shares = "document.getElementById('form-shares').setAttribute('style', 'inline-block')"
+    _java_script_forex = "document.getElementById('form-forex').setAttribute('style', 'inline-block')"
+    _java_script_cfds = "document.getElementById('form-cfds').setAttribute('style', 'inline-block')"
+    _java_script_spread_betting = "document.getElementById('form-spread_betting').setAttribute('style', 'inline-block')"
+    _java_script_relevante_experience = "document.getElementById('form-relevant_experience').setAttribute('style', 'inline-block')"
+    _java_script_trading_platform = "document.getElementById('form-trading_accounts').setAttribute('style', 'inline-block')"
+    _java_script_currency = "document.getElementById('form-currency').setAttribute('style', 'inline-block')"
+    _java_script_annual_income = "document.getElementById('form-approx_annual_income').setAttribute('style', 'inline-block')"
+    _java_script_employment_status = "document.getElementById('form-employment_status').setAttribute('style', 'inline-block')"
+    _java_script_assets = "document.getElementById('form-liquid_savings').setAttribute('style', 'inline-block')"
--- features/steps/questionnaire_steps.py
+++ features/steps/questionnaire_steps.py
@@ -4,120 +4,121 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-from features.pages.sign_up_page import Locators, Elements
+from features.pages.sign_up_page import Locators
+from features.pages.questionnaire_page import Locators_q
 
 @then('User is redirected to a questionnaire')
 def step_impl(context):
     #WebDriverWait(context.browser, 15).until(EC.element_to_be_clickable((By.XPATH, "[ng-class='\{\'text-red\'\:field\.\$invalid \&\& form\.\$submitted\}']")))
-    title = context.browser.find_element(By.XPATH, Locators._path_question_second_page)
+    title = context.browser.find_element(By.XPATH, Locators_q._path_question_second_page)
     assert title.is_displayed()
 
 @when('Select one of "{options}" from Shares')
 def step_impl(context, options):
-    WebDriverWait(context.browser, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, Locators._path_finish_button)))
-    context.browser.execute_script(Locators._java_script_shares)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_shares))
+    WebDriverWait(context.browser, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, Locators_q._path_finish_button)))
+    context.browser.execute_script(Locators_q._java_script_shares)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_shares))
     dd.select_by_visible_text(options)
 
 @then('One of the Shares "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_shares))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_shares))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Forex')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_forex)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_forex))
+    context.browser.execute_script(Locators_q._java_script_forex)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_forex))
     dd.select_by_visible_text(options)
 
 @then('One of the Forex "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_forex))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_forex))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Cfds')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_cfds)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_cfds))
+    context.browser.execute_script(Locators_q._java_script_cfds)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_cfds))
     dd.select_by_visible_text(options)
 
 @then('One of the Cfds "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_cfds))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_cfds))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Spread betting')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_spread_betting)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_spread_betting))
+    context.browser.execute_script(Locators_q._java_script_spread_betting)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_spread_betting))
     dd.select_by_visible_text(options)
 
 @then('One of the Spread betting "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_spread_betting))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_spread_betting))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Relevante Experience')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_relevante_experience)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_relevante_experience))
+    context.browser.execute_script(Locators_q._java_script_relevante_experience)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_relevante_experience))
     dd.select_by_visible_text(options)
 
 @then('One of the Relevante Experience "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_relevante_experience))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_relevante_experience))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{answer}" from Trading platform')
 def step_impl(context, answer):
-    context.browser.execute_script(Locators._java_script_trading_platform)
-    element = context.browser.find_element(By.XPATH, Locators._path_trading_platform)
+    context.browser.execute_script(Locators_q._java_script_trading_platform)
+    element = context.browser.find_element(By.XPATH, Locators_q._path_trading_platform)
     element.send_keys(answer)
 @then('One of the Trading platform "{answer}" is visible')
 def step_impl(context, answer):
-    dd = Select(context.browser.find_element(By.XPATH, Locators._path_trading_platform))
+    dd = Select(context.browser.find_element(By.XPATH, Locators_q._path_trading_platform))
     assert  dd.first_selected_option.text == answer
 
 @when('Select one of "{options}" from Currency')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_currency)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_currency))
+    context.browser.execute_script(Locators_q._java_script_currency)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_currency))
     dd.select_by_visible_text(options)
 
 @then('One of the Currency "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_currency))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_currency))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Annual Income')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_annual_income)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_annual_income))
+    context.browser.execute_script(Locators_q._java_script_annual_income)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_annual_income))
     dd.select_by_visible_text(options)
 
 @then('One of the Annual Income "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_annual_income))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_annual_income))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Employment status')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_employment_status)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_employment_status))
+    context.browser.execute_script(Locators_q._java_script_employment_status)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_employment_status))
     dd.select_by_visible_text(options)
 
 @then('One of the Employment status "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_employment_status))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_employment_status))
     assert dd.first_selected_option.text == options
 
 @when('Select one of "{options}" from Assets')
 def step_impl(context, options):
-    context.browser.execute_script(Locators._java_script_assets)
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_assets))
+    context.browser.execute_script(Locators_q._java_script_assets)
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_assets))
     dd.select_by_visible_text(options)
 
 @then('One of the Assets "{options}" is visible')
 def step_impl(context, options):
-    dd = Select (context.browser.find_element(By.XPATH, Locators._path_assets))
+    dd = Select (context.browser.find_element(By.XPATH, Locators_q._path_assets))
     assert dd.first_selected_option.text == options
